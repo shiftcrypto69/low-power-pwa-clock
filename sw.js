3fb57bf7e9c4a89c25397cfe7db6aced27298ccf
ShiftDev.js <35767816+shiftcrypto69@users.noreply.github.com>
2026-02-09 19:42:12 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,28 @@
-const cacheName = 'clock-v1';
+const cacheName = 'grey-glow-v2'; // Nama versi baru
 const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+// Pasang Service Worker dan simpan cache
+self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Paksa SW baru aktif serta-merta
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Padam cache lama secara automatik
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Ambil fail dari cache, jika tiada baru guna network
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
